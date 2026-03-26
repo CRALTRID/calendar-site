6451cf1101b282f3c71ecaa1c1b4f5c8c0ace4e1
CRALTRID <crealtrid@gmail.com>
2026-03-25 23:38:08 -0600

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,9 +1,11 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
 import { firebaseConfig } from "./config.js";
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-export { app, auth, provider };
+export { app, db, auth, provider };
