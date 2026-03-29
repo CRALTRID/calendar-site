4664c01988e9a57b52c0a7507225a0b430b8294d
CRALTRID <crealtrid@gmail.com>
2026-03-28 19:36:47 -0600

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,11 +1,18 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
-import { firebaseConfig } from "./config.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "你的key",
+  authDomain: "你的domain",
+  projectId: "你的projectId",
+  storageBucket: "你的bucket",
+  messagingSenderId: "xxx",
+  appId: "xxx"
+};
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
 
-export { app, db, auth, provider };
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
