a58e8dd17d0948ba1225403098e3c17abad317e9
CRALTRID <crealtrid@gmail.com>
2026-03-28 19:47:12 -0600

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,22 +1,19 @@
 import {
   signInWithPopup,
   signOut,
-  onAuthStateChanged,
-  setPersistence,
-  browserLocalPersistence
-} from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
 import { auth, provider } from "./firebase.js";
 
-export async function loginWithGoogle() {
-  await setPersistence(auth, browserLocalPersistence);
+export function loginWithGoogle() {
   return signInWithPopup(auth, provider);
 }
 
-export async function logoutUser() {
+export function logoutUser() {
   return signOut(auth);
 }
 
-export function watchAuthState(callback) {
-  return onAuthStateChanged(auth, callback);
+export function watchAuthState(cb) {
+  onAuthStateChanged(auth, cb);
 }
