3fe418dfe66c82947fa8dc104adce5cf97d73064
CRALTRID <crealtrid@gmail.com>
2026-03-28 19:47:55 -0600

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,16 +1,17 @@
-import { db } from "./firebase.js";
 import {
   doc,
   setDoc,
-  getDoc
-} from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
+  getDoc,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+import { db } from "./firebase.js";
 
 export async function saveUserData(uid, data) {
-  await setDoc(doc(db, "users", uid), data);
+  await setDoc(doc(db, "users", uid), data, { merge: true });
 }
 
 export async function loadUserData(uid) {
-  const docRef = doc(db, "users", uid);
-  const snap = await getDoc(docRef);
-  return snap.exists() ? snap.data() : null;
+  const snap = await getDoc(doc(db, "users", uid));
+  return snap.exists() ? snap.data() : {};
 }
